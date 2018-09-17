328ce7ec466f6d2c06d1aa037ec5403fff18c081
scavenger2 <liverkiller@gmail.com>
2018-09-18 01:18:11 +0800

    update config & doc of istanbul

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,34 @@
+var path = require('path');
 var webpackCommonConfig = require('./webpack.common.js');
 webpackCommonConfig['mode']='development';
 delete webpackCommonConfig["output"];
 delete webpackCommonConfig["optimization"];
 webpackCommonConfig['devtool']="inline-source-map";
+var srcMatcher=webpackCommonConfig['module']['rules'][0];
+webpackCommonConfig['module']['rules'].push({
+    test: srcMatcher['test'],
+    include: path.resolve('src'),
+    exclude: srcMatcher['exclude'],
+    use: {
+        loader: 'istanbul-instrumenter-loader',
+        options: {
+            esModules: true
+        }
+    },
+    enforce: 'post'
+});
 module.exports = function(config){
     config.set({
         basePath: "./test",
         files: [
-            "./webpack.test.js"
+            "./webpack.test.js",
+            "./source.test.js"
         ],
         exclude: [],
         frameworks: ["jasmine"], 
         preprocessors: {
-            './webpack.test.js': ['webpack', 'sourcemap']
+            './webpack.test.js': ['webpack', 'sourcemap'],
+            './source.test.js': ['coverage', 'webpack']
         },
 
         colors: true,
@@ -26,8 +42,15 @@ module.exports = function(config){
         port: 4000,
         urlRoot: "/karma",
 
-        reporters: ['kjhtml'],
+        reporters: ['kjhtml', 'coverage-istanbul'],
 
-        webpack: webpackCommonConfig
+        webpack: webpackCommonConfig,
+
+        coverageIstanbulReporter: {
+            reports: ['html'],
+            dir: path.join(__dirname, 'coverage'),
+            fixWebpackSourcePaths: true,
+            skipFilesWithNoCoverage: true
+        }
     });
 }
